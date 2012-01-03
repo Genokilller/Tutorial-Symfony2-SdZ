0dc6cb4eb45485308026b00f2d394c3cb583c955
Vincent Paulin <vincent.paulin@creepykiwi.com>
2012-01-04 00:58:11 +0100

    Updates for the second TP : discover controllers functionnalities

--- Symfony/src/tuto/WelcomeBundle/Controller/HomepageController.php
+++ Symfony/src/tuto/WelcomeBundle/Controller/HomepageController.php
@@ -7,9 +7,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomepageController extends Controller
 {
-    
+
     public function indexAction()
     {
+        $this->get('session')->setFlash('notice', 'Bienvenue à toi '.$this->get('session')->get('user_name').' !');
+
         return $this->render('tutoWelcomeBundle:Homepage:index.html.twig');
+    }
+
+    public function whoAmIAction($name)
+    {
+        # get the session
+        $session = $this->get('session');
+
+        # store the user'name in the session
+        $session->set('user_name', $name);
+
+        return $this->redirect($this->container->get('router')->generate('tutoWelcomeBundle_homepage'));
     }
 }
